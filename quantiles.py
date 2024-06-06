a89a3fc683a5ff4816c147e0cdad4975c9c10930
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2024-06-06 20:40:53 +0200

    feat: comment out deprecated function

--- quantiles.py
+++ quantiles.py
@@ -69,78 +69,70 @@ def plot_quantiles(
     return all_lines
 
 
-def plot_approximations(
-    evaluate_function,
-    evaluate_basis,
-    reconstruct,
-    ax,
-    numTrials=10_000,
-    title=None,
-    cachePath=None,
-):
-    C0 = coloring.mix(coloring.bimosred, 80)
-    C1 = "xkcd:black"
+# NOTE: The subsequent function is deprecated, since it is not purely related to plotting.
 
-    cs = []
-    es = []
-    if cachePath is not None and os.path.isfile(cachePath):
-        z = np.load(cachePath)
-        cs = list(z["cs"])
-        es = list(z["es"])
-        assert len(cs) == len(es)
+# import os
+# from tqdm import tqdm
 
-    with tqdm(total=numTrials, initial=len(cs), desc=f"Reconstruct '{title}'") as pbar:
-        while len(cs) < numTrials:
-            try:
-                c, e = reconstruct()
-                cs.append(c)
-                es.append(e)
-                pbar.update()
-            except np.linalg.LinAlgError:
-                pass  # Condition on the event that the problem is well-conditioned.
-            except KeyboardInterrupt:
-                break
-            # except: pass  # Errors in the optimizer... TODO: make more specific
-    assert len(cs) == len(es) or len(cs) == len(es) + 1
-    cs = cs[: len(es)]
-    cs, es = np.array(cs, dtype=float), np.array(es, dtype=float)
+# def plot_approximations(
+#     evaluate_function, evaluate_basis, reconstruct, ax, numTrials=10_000, title=None, cachePath=None,
+# ):
+#     C0 = coloring.mix(coloring.bimosred, 80)
+#     C1 = "xkcd:black"
 
-    if cachePath is not None:
-        np.savez_compressed(cachePath, cs=cs, es=es)
+#     cs = []
+#     es = []
+#     if cachePath is not None and os.path.isfile(cachePath):
+#         z = np.load(cachePath)
+#         cs = list(z["cs"])
+#         es = list(z["es"])
+#         assert len(cs) == len(es)
 
-    xs = np.linspace(-1, 1, 1000)
-    fxs = evaluate_function(xs)
-    measures = evaluate_basis(xs, np.eye(cs.shape[1]))
-    assert measures.shape == (len(xs), cs.shape[1])
-    yss = cs @ measures.T
-    assert yss.shape == (cs.shape[0], len(xs))
+#     with tqdm(total=numTrials, initial=len(cs), desc=f"Reconstruct '{title}'") as pbar:
+#         while len(cs) < numTrials:
+#             try:
+#                 c, e = reconstruct()
+#                 cs.append(c)
+#                 es.append(e)
+#                 pbar.update()
+#             except np.linalg.LinAlgError:
+#                 pass  # Condition on the event that the problem is well-conditioned.
+#             except KeyboardInterrupt:
+#                 break
+#             # except: pass  # Errors in the optimizer... TODO: make more specific
+#     assert len(cs) == len(es) or len(cs) == len(es) + 1
+#     cs = cs[: len(es)]
+#     cs, es = np.array(cs, dtype=float), np.array(es, dtype=float)
 
-    numQuantiles = 500
-    plot_quantiles(xs, yss, num_quantiles=numQuantiles, axes=ax, color=C1, linewidth=0)
-    ax.plot(
-        xs,
-        fxs,
-        linestyle=(0, (0.25, 1.5)),
-        color=C0,
-        linewidth=3,
-        dash_capstyle="round",
-    )
-    ymin = np.min(fxs) - (np.max(fxs) - np.min(fxs)) / 4
-    ymax = np.max(fxs) + (np.max(fxs) - np.min(fxs)) / 4
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(ymin, ymax)
+#     if cachePath is not None:
+#         np.savez_compressed(cachePath, cs=cs, es=es)
 
-    if title is not None:
-        # errors = np.max(abs(yss - fxs[None]), axis=1)
-        errors = np.sqrt(np.trapz((yss - fxs[None]) ** 2, xs, axis=1))
-        info = f"(avg.\ min.\ eigenvalue: {np.mean(es):.2e}, avg.\ error: {np.mean(errors):.2e})"
-        ax.set_title(
-            r"{\fontsize{15pt}{18pt}\selectfont{}"
-            + title
-            + r"}"
-            + "\n"
-            + r"{\fontsize{10pt}{12pt}\selectfont{}"
-            + info
-            + "}",
-            multialignment="center",
-        )
+#     xs = np.linspace(-1, 1, 1000)
+#     fxs = evaluate_function(xs)
+#     measures = evaluate_basis(xs, np.eye(cs.shape[1]))
+#     assert measures.shape == (len(xs), cs.shape[1])
+#     yss = cs @ measures.T
+#     assert yss.shape == (cs.shape[0], len(xs))
+
+#     numQuantiles = 500
+#     plot_quantiles(xs, yss, num_quantiles=numQuantiles, axes=ax, color=C1, linewidth=0)
+#     ax.plot(xs, fxs, linestyle=(0, (0.25, 1.5)), color=C0, linewidth=3, dash_capstyle="round")
+#     ymin = np.min(fxs) - (np.max(fxs) - np.min(fxs)) / 4
+#     ymax = np.max(fxs) + (np.max(fxs) - np.min(fxs)) / 4
+#     ax.set_xlim(-1, 1)
+#     ax.set_ylim(ymin, ymax)
+
+#     if title is not None:
+#         # errors = np.max(abs(yss - fxs[None]), axis=1)
+#         errors = np.sqrt(np.trapz((yss - fxs[None]) ** 2, xs, axis=1))
+#         info = f"(avg.\ min.\ eigenvalue: {np.mean(es):.2e}, avg.\ error: {np.mean(errors):.2e})"
+#         ax.set_title(
+#             r"{\fontsize{15pt}{18pt}\selectfont{}"
+#             + title
+#             + r"}"
+#             + "\n"
+#             + r"{\fontsize{10pt}{12pt}\selectfont{}"
+#             + info
+#             + "}",
+#             multialignment="center",
+#         )
