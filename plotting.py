2371a452d0e4379d47b4be216c6e1b84a2fd23e6
Philipp Trunschke <philipp.trunschke@univ-nantes.fr>
2025-03-05 09:45:37 +0100

    feat!: provide PlotStyle to save_figure

--- plotting.py
+++ plotting.py
@@ -1,9 +1,11 @@
 import os
+import typing as t
 from contextlib import contextmanager
+from pathlib import Path
 
-import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def mix(*color_value_list, alpha=True):
@@ -45,8 +47,69 @@ block_body_bg = mix(block_title_bg, 50, normal_text_bg)
 frametitle_fg = mix(bimosred, 75, normal_text_fg)
 
 
+class PlotStyle(t.Protocol):
+    @property
+    def geometry(self) -> dict[str, float]:
+        pass
+
+    @property
+    def fg(self) -> str:
+        pass
+
+    @property
+    def bg(self) -> str:
+        pass
+
+    @property
+    def font(self) -> dict[str, t.Any]:
+        pass
+
+    def set(self) -> None:
+        pass
+
+
+class DarkMode(object):
+    def __init__(self) -> None:
+        self.geometry = {
+            "top": 1,
+            "bottom": 0,
+            "left": 0,
+            "right": 1,
+            "wspace": 0.25,  # the default as defined in rcParams
+            "hspace": 0.25,  # the default as defined in rcParams
+        }
+        self.fg = "#FFFFFF"
+        self.bg = "#1F1F1F"
+        self.font = {
+            "family": "serif",
+            "weight": "ultralight",
+            "size": 10,
+        }
+
+    def set(self) -> None:
+        mpl.rc("font", **self.font)
+        # mpl.rc("text", usetex=True)
+        # mpl.rc(
+        #     "text.latex",
+        #     preamble=r"""
+        #     \usepackage{amsmath}
+        #     \usepackage{bbm}
+        # """,
+        # )
+        mpl.rc("text", color=self.fg)
+        mpl.rc("figure", facecolor=self.bg, edgecolor=self.bg)
+        mpl.rc(
+            "axes",
+            facecolor=self.bg,
+            edgecolor=self.fg,
+            labelcolor=self.fg,
+        )
+        mpl.rc("xtick", color=self.fg)
+        mpl.rc("ytick", color=self.fg)
+
+
 class BIMoSStyle(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.geometry = {
             "top": 1,
             "bottom": 0,
@@ -57,7 +120,7 @@ class BIMoSStyle(object):
         }
         self.fontSize = 10
 
-    def set(self):
+    def set(self) -> None:
         mpl.rc("font", size=self.fontSize, family="Times New Roman")
         mpl.rc("text", usetex=True)
         mpl.rc(
@@ -69,9 +132,70 @@ class BIMoSStyle(object):
         """,
         )
         mpl.rc("figure", facecolor=normal_text_bg, edgecolor=normal_text_bg)
-        mpl.rc("axes", facecolor=block_body_bg, edgecolor=normal_text_fg, labelcolor=normal_text_fg)
+        mpl.rc(
+            "axes",
+            facecolor=block_body_bg,
+            edgecolor=normal_text_fg,
+            labelcolor=normal_text_fg,
+        )
         mpl.rc("xtick", color=normal_text_fg)
         mpl.rc("ytick", color=normal_text_fg)
+
+
+@contextmanager
+def save_figure(
+    output_path: Path,
+    *fig_shape: int,
+    fig_width: float = 6.52437486112,
+    plot_style: PlotStyle,
+) -> t.Iterator[tuple[plt.Figure, plt.Axes]]:
+    """
+    Context manager to save a figure to a file.
+
+    Parameters
+    ----------
+    filename : str
+    plot_style : PlotStyle
+    figwidth : float, optional
+        Width of the figure in inches, by default 6.52437486112
+    """
+    plot_style.set()
+
+    assert len(fig_shape) <= 2
+    if len(fig_shape) == 0:
+        fig_shape = (1, 1)
+    elif len(fig_shape) == 1:
+        fig_shape = (1, fig_shape[0])
+
+    phi = (1 + np.sqrt(5)) / 2
+    aspect_ratio = phi
+    figsize = compute_figsize(
+        plot_style.geometry, fig_shape, aspect_ratio=aspect_ratio, figwidth=fig_width
+    )
+
+    try:
+        fig, ax = plt.subplots(*fig_shape, figsize=figsize, dpi=300)
+        fig.patch.set_facecolor(plot_style.bg)
+        if isinstance(ax, np.ndarray):
+            for axi in ax.ravel():
+                axi.set_facecolor(plot_style.bg)
+        else:
+            ax.set_facecolor(plot_style.bg)
+        yield fig, ax
+    finally:
+        plt.subplots_adjust(**plot_style.geometry)
+        dirname = os.path.dirname(output_path)
+        if dirname:
+            os.makedirs(dirname, exist_ok=True)
+        plt.savefig(
+            output_path,
+            format="png",
+            facecolor=fig.get_facecolor(),
+            edgecolor="none",
+            bbox_inches="tight",
+            dpi=300,
+        )
+        plt.close(fig)
 
 
 def homotopy(x, y, num):
@@ -85,7 +209,9 @@ def homotopy(x, y, num):
 
 def compute_figsize(geometry, shape, aspect_ratio=1, figwidth=3.98584):
     subplotwidth = (
-        (geometry["right"] - geometry["left"]) * figwidth / (shape[1] + (shape[1] - 1) * geometry["wspace"])
+        (geometry["right"] - geometry["left"])
+        * figwidth
+        / (shape[1] + (shape[1] - 1) * geometry["wspace"])
     )  # make as wide as two plots
     subplotheight = subplotwidth / aspect_ratio
     figheight = subplotheight * (shape[0] + (shape[0] - 1) * geometry["hspace"])
@@ -93,44 +219,46 @@ def compute_figsize(geometry, shape, aspect_ratio=1, figwidth=3.98584):
     return (figwidth, figheight)
 
 
-@contextmanager
-def save_figure(filename: str, *figshape):
-    BG = "xkcd:white"
-    geometry = BIMoSStyle().geometry
+# @contextmanager
+# def save_figure(filename: str, *figshape):
+#     BG = "xkcd:white"
+#     geometry = BIMoSStyle().geometry
 
-    assert len(figshape) <= 2
-    if len(figshape) == 0:
-        figshape = (1, 1)
-    elif len(figshape) == 1:
-        figshape = (1, figshape[0])
+#     assert len(figshape) <= 2
+#     if len(figshape) == 0:
+#         figshape = (1, 1)
+#     elif len(figshape) == 1:
+#         figshape = (1, figshape[0])
 
-    mpl.rc("font", size=10, family="Times New Roman")
-    mpl.rc("text", usetex=True)
+#     mpl.rc("font", size=10, family="Times New Roman")
+#     mpl.rc("text", usetex=True)
 
-    figwidth = 6.52437486112  # width of the figure in inches
-    phi = (1 + np.sqrt(5)) / 2
-    aspect_ratio = phi
-    figsize = compute_figsize(geometry, figshape, aspect_ratio=aspect_ratio, figwidth=figwidth)
+#     figwidth = 6.52437486112  # width of the figure in inches
+#     phi = (1 + np.sqrt(5)) / 2
+#     aspect_ratio = phi
+#     figsize = compute_figsize(
+#         geometry, figshape, aspect_ratio=aspect_ratio, figwidth=figwidth
+#     )
 
-    try:
-        fig, ax = plt.subplots(*figshape, figsize=figsize, dpi=300)
-        fig.patch.set_facecolor(BG)
-        if isinstance(ax, np.ndarray):
-            for axi in ax.ravel():
-                axi.set_facecolor(BG)
-        else:
-            ax.set_facecolor(BG)
-        yield fig, ax
-    finally:
-        plt.subplots_adjust(**geometry)
-        dirname = os.path.dirname(filename)
-        if dirname:
-            os.makedirs(dirname, exist_ok=True)
-        plt.savefig(
-            filename,
-            format="png",
-            facecolor=fig.get_facecolor(),
-            edgecolor="none",
-            bbox_inches="tight",
-        )
-        plt.close(fig)
+#     try:
+#         fig, ax = plt.subplots(*figshape, figsize=figsize, dpi=300)
+#         fig.patch.set_facecolor(BG)
+#         if isinstance(ax, np.ndarray):
+#             for axi in ax.ravel():
+#                 axi.set_facecolor(BG)
+#         else:
+#             ax.set_facecolor(BG)
+#         yield fig, ax
+#     finally:
+#         plt.subplots_adjust(**geometry)
+#         dirname = os.path.dirname(filename)
+#         if dirname:
+#             os.makedirs(dirname, exist_ok=True)
+#         plt.savefig(
+#             filename,
+#             format="png",
+#             facecolor=fig.get_facecolor(),
+#             edgecolor="none",
+#             bbox_inches="tight",
+#         )
+#         plt.close(fig)
